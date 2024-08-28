03b2c79927f2affdc2683956a18a6fdb568b1b4b
Sergei Mikurov <454004@mail.ru>
2024-08-29 00:08:35 +0500

    feat(app): start build app

--- wselfedu/src/wselfedu/app.py
+++ wselfedu/src/wselfedu/app.py
@@ -7,53 +7,15 @@ from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
 
-def greeting(name):
-    if name:
-        return f"Hello, {name}"
-    else:
-        return "Hello, stranger"
-
-
 class WebSelfEducation(toga.App):
     """Web self education application class."""
 
     def startup(self):
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        name_label = toga.Label(
-            "Your name: ",
-            style=Pack(padding=(0, 5)),
-        )
-        self.name_input = toga.TextInput(style=Pack(flex=1))
-
-        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        name_box.add(name_label)
-        name_box.add(self.name_input)
-
-        button = toga.Button(
-            "Say Hello!",
-            on_press=self.say_hello,
-            style=Pack(padding=5),
-        )
-
-        main_box.add(name_box)
-        main_box.add(button)
-
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
-
-    def say_hello(self, widget):
-        with httpx.Client() as client:
-            response = client.get(
-                "https://jsonplaceholder.typicode.com/posts/42")
-
-        payload = response.json()
-
-        self.main_window.info_dialog(
-            greeting(self.name_input.value),
-            payload["body"],
-        )
 
 def main():
     return WebSelfEducation()
