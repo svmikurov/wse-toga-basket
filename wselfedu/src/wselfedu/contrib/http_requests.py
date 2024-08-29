049a065352b4fb3264f4ecd8603473340b82afcc
Sergei Mikurov <454004@mail.ru>
2024-08-29 11:16:29 +0500

    refator(http): apply httpx.BasicAuth

--- wselfedu/src/wselfedu/contrib/http_requests.py
+++ wselfedu/src/wselfedu/contrib/http_requests.py
@@ -3,7 +3,6 @@
 from urllib.parse import urljoin
 
 import httpx
-from httpx import Request
 
 DEV_HOST_API = 'http://127.0.0.1/'
 DEV_USERNAME = 'user1'
@@ -14,42 +13,7 @@ USERNAME = DEV_USERNAME
 PASSWORD = DEV_PASSWORD
 GET_TOKEN_PATH = 'auth/token/login/'
 
-
-class CustomAuth(httpx.Auth):
-    """Custom authentication class with httpx library."""
-
-    _access_token = None
-
-    def __init__(self, username: str, password: str) -> None:
-        """Construct auth."""
-        self.auth_data = {
-            'username': username,
-            'password': password,
-        }
-
-    def auth_flow(self, request: Request) -> Request:
-        """Add headers to request."""
-        request.headers['Authorization'] = f'Token {self.access_token}'
-        yield request
-
-    @property
-    def access_token(self) -> str:
-        """Access token."""
-        if not self._access_token:
-            self.get_access_token()
-        return self._access_token
-
-    def get_access_token(self) -> None:
-        """Get access token."""
-        with httpx.Client() as client:
-            response = client.post(
-                url=urljoin(HOST_API, GET_TOKEN_PATH),
-                json=self.auth_data,
-            )
-        self._access_token = response.json()['auth_token']
-
-
-auth = CustomAuth(USERNAME, PASSWORD)
+auth = httpx.BasicAuth(USERNAME, PASSWORD)
 
 
 def send_get_request(path: str, url: str | None = None) -> list[dict]:
