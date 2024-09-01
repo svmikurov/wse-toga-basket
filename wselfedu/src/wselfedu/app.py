6386a550decc46f286251fb7b7860259bfa14c47
Sergei Mikurov <454004@mail.ru>
2024-09-01 17:40:51 +0500

    feat(teat): add tutorial tests

--- wselfedu/src/wselfedu/app.py
+++ wselfedu/src/wselfedu/app.py
@@ -9,7 +9,10 @@ from toga.style.pack import COLUMN, ROW
 
 def greeting(name):
     if name:
-        return f"Hello, {name}"
+        if name == "Brutus":
+            return "BeeWare the IDEs of Python!"
+        else:
+            return f"Hello, {name}"
     else:
         return "Hello, stranger"
 
--- wselfedu/tests/test_app.py
+++ wselfedu/tests/test_app.py
@@ -1,3 +1,21 @@
+from wselfedu.app import greeting
+
+
 def test_first():
     """An initial test for the app."""
     assert 1 + 1 == 2
+
+
+def test_name():
+    """If a name is provided, the greeting includes the name"""
+    assert greeting("Alice") == "Hello, Alice"
+
+
+def test_empty():
+    """If a name is not provided, a generic greeting is provided"""
+    assert greeting("") == "Hello, stranger"
+
+
+def test_brutus():
+    """If the name is Brutus, a special greeting is provided"""
+    assert greeting("Brutus") == "BeeWare the IDEs of Python!"
